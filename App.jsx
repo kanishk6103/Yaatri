b423a6808cc1c275872b2088267fc1438e84ea7f
kanishk6103 <mamta11tiwari06@gmail.com>
2024-05-05 14:52:21 +0530

    initialised nativewind

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import "./global.css"
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-7xl">Rail Yaatri!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
