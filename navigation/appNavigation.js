24f497eb64d4079c8a3bef55ed22745de5f97799
atharva <atharva.sugandhi@gmail.com>
2024-05-05 22:34:57 +0530

    Home Page, SVG --> Component, UseNavigator

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
-import PNRCheckScreen from "../screens/PNRCheckScreen"
-import TrainRunningScreen from "../screens/TrainRunningScreen.jsx"
-import FindTrainsScreen from "../screens/FindTrainsScreen.jsx"
-// const Tab = createBottomTabNavigator();
+import PNRCheckScreen from "../screens/PNRCheckScreen";
+import TrainRunningScreen from "../screens/TrainRunningScreen.jsx";
+import FindTrainsScreen from "../screens/FindTrainsScreen.jsx";
 const Stack = createNativeStackNavigator();
 const MyStack = () => {
     return (
-        <Stack.Navigator
-            initialRouteName="Home"
-        >
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen
-                name="PNR"
-                component={PNRCheckScreen}
-            />
-            <Stack.Screen
-                name="TrainStatus"
-                component={TrainRunningScreen}
-            />
-            <Stack.Screen
-                name="FindTrains"
-                component={FindTrainsScreen}
-            />
+        <Stack.Navigator initialRouteName="Yaatri">
+            <Stack.Screen name="Yaatri" component={HomeScreen} />
+            <Stack.Screen name="PNR" component={PNRCheckScreen} />
+            <Stack.Screen name="TrainStatus" component={TrainRunningScreen} />
+            <Stack.Screen name="FindTrains" component={FindTrainsScreen} />
         </Stack.Navigator>
     );
-}
+};
 const AppNavigator = () => {
     return (
         <NavigationContainer>
