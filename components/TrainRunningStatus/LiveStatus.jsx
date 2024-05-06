3cf1ef6a4ef65a2e52a31f271efdf69915207692
atharva <atharva.sugandhi@gmail.com>
2024-05-06 13:57:36 +0530

    Live Status Coming Soon

---
+++ components/TrainRunningStatus/LiveStatus.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LiveStatus = () => {
+    return (
+        <View className="flex justify-center items-center w-screen h-screen absolute">
+            <Text className="text-white mx-8 text-4xl">
+                Coming Soon...
+            </Text>
+        </View>
+    );
+};
+
+export default LiveStatus;
--- screens/TrainRunningScreen.jsx
+++ screens/TrainRunningScreen.jsx
@@ -1,13 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Button } from 'react-native'
-const TrainRunningScreen = ({ navigation }) => {
-    return (
-        <View>
-            <Text className=''>TrainRunningScreen</Text>
-            <Button title="Go back" onPress={() => navigation.goBack()} />
-        </View>
-    )
-}
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Button } from "react-native";
+import Input from "../components/TrainRunningStatus/Input.jsx";
+import LiveStatus from "../components/TrainRunningStatus/LiveStatus.jsx";
 
-export default TrainRunningScreen
+const TrainRunningScreen = ({ navigation }) => {
+    const [clicked, setClicked] = useState(true);
+    const handleClick = () => {
+        setClicked(true);
+    };
+
+    return (
+        <View className="w-screen h-screen bg-Background">
+            <View className="mx-8">
+                <Text className="text-lg font-semibold text-textWhite my-5">
+                    Enter Train Number
+                </Text>
+                <Input value={"TRAIN"} />
+            </View>
+
+            <LiveStatus />
+        </View>
+    );
+};
+
+export default TrainRunningScreen;
